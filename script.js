c3929b18c65933399c6d099856fc05fe09e7d39a
saulwade <saulwade29@gmail.com>
2023-02-13 20:30:49 -0600

    edited html,js and readme file

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 
+// Defining variables
+
+var timeBlocks = $('.container');
+var timeBlockRow = $('.time-block');
+var isPresent;
+var isPast;
+var isFuture;
+
+
+var todayDate = moment().format('dddd, MMMM Do');
+$('#currentDay').text(todayDate);
+
+var saveBtnEl = $('.saveBtn');
+
+saveBtnEl.on('click', function (event) {
+    event.preventDefault()
+
+
+    var time1 = $(this).parent().parent().data('time')
+
+    var descriptionBox = $(this).siblings('.description').val();
+
+    localStorage.setItem('time', time1);
+    localStorage.setItem('description', descriptionBox)
+});
+
+function colorDate() {
+    var currentHour = moment().hour()
+
+    timeBlockRow.each(function () {
+
+
+        var blockHour = parseInt($(this).data('time'));
+
+
+        if (currentHour === blockHour) {
+            $(this).addClass('present')
+
+        } else if (currentHour > blockHour) {
+            $(this).addClass('past')
+        }
+        else {
+            $(this).addClass('future')
+        }
+    });
+}
+
+
